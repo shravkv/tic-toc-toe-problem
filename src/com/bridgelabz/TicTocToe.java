0728228603b9fc1098dcb7a3152c0b4b7a7ba618
shravkv <sravan.k.vodnala@gmail.com>
2022-07-09 14:32:48 +0530

    Added: Created tic tac toe game board

--- src/com/bridgelabz/TicTocToe.java
+++ src/com/bridgelabz/TicTocToe.java
@@ -2,9 +2,16 @@ package com.bridgelabz;
 
 public class TicTocToe {
 
+    static char[] gameBoard = new char[10];
+
     public static void main(String[] args) {
 
         System.out.println("Welcome to Tic Tac Toe game");
-
+        createBoard();
+    }
+    public static void createBoard() {
+        System.out.println(gameBoard[0] + " | " + gameBoard[1] + " | " + gameBoard[2]);
+        System.out.println(gameBoard[3] + " | " + gameBoard[4] + " | " + gameBoard[5]);
+        System.out.println(gameBoard[6] + " | " + gameBoard[7] + " | " + gameBoard[8]);
     }
 }
