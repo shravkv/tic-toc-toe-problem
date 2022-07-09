3e01f9dc21c4965b40cd5ef662367b3d765800d8
shravkv <sravan.k.vodnala@gmail.com>
2022-07-09 14:47:16 +0530

    Added: User can make choice of X or 0

--- src/com/bridgelabz/TicTocToe.java
+++ src/com/bridgelabz/TicTocToe.java
@@ -1,17 +1,25 @@
 package com.bridgelabz;
 
-public class TicTocToe {
+import java.util.Scanner;
 
+public class TicTocToe {
     static char[] gameBoard = new char[10];
+    static Scanner scanner = new Scanner(System.in);
 
     public static void main(String[] args) {
 
         System.out.println("Welcome to Tic Tac Toe game");
         createBoard();
+        user();
+
     }
     public static void createBoard() {
         System.out.println(gameBoard[0] + " | " + gameBoard[1] + " | " + gameBoard[2]);
         System.out.println(gameBoard[3] + " | " + gameBoard[4] + " | " + gameBoard[5]);
         System.out.println(gameBoard[6] + " | " + gameBoard[7] + " | " + gameBoard[8]);
+    }
+    public static char user() {
+        System.out.println("Enter your choice  X or O only");
+        return scanner.next().toUpperCase().charAt(0);
     }
 }
