40c3e110abf08bc3a6042734f16812b86b03fa05
shravkv <sravan.k.vodnala@gmail.com>
2022-07-09 15:11:57 +0530

    Added: User to make a move to desired location

--- src/com/bridgelabz/TicTocToe.java
+++ src/com/bridgelabz/TicTocToe.java
@@ -5,6 +5,9 @@ import java.util.Scanner;
 public class TicTocToe {
     static char[] gameBoard = new char[10];
     static Scanner scanner = new Scanner(System.in);
+    static char user = '1';
+    static int userPosition;
+    static char userTurn;
 
     public static void main(String[] args) {
 
@@ -12,6 +15,7 @@ public class TicTocToe {
         ticTacToe();
         createBoard();
         user();
+        moveLocation();
 
     }
     public static void createBoard() {
@@ -26,6 +30,63 @@ public class TicTocToe {
     public static void ticTacToe() {
         for (int i = 0; i < gameBoard.length; i++) {
             gameBoard[i] = ' ';
+        }
+    }
+    public static void moveLocation() {
+        if (user == '1') {
+            System.out.print("Select the position in board from range [0 - 8] : ");
+            userPosition = scanner.nextInt();
+
+            switch (userPosition) {
+                case 0:
+                    if (gameBoard[0] == ' ') {
+                        gameBoard[0] = userTurn;
+                    }
+                    break;
+                case 1:
+                    if (gameBoard[1] == ' ') {
+                        gameBoard[1] = userTurn;
+                    }
+                    break;
+                case 2:
+                    if (gameBoard[2] == ' ') {
+                        gameBoard[2] = userTurn;
+                    }
+                    break;
+                case 3:
+                    if (gameBoard[3] == ' ') {
+                        gameBoard[3] = userTurn;
+                    }
+                    break;
+                case 4:
+                    if (gameBoard[4] == ' ') {
+                        gameBoard[4] = userTurn;
+                    }
+                    break;
+                case 5:
+                    if (gameBoard[5] == ' ') {
+                        gameBoard[5] = userTurn;
+                    }
+                    break;
+                case 6:
+                    if (gameBoard[6] == ' ') {
+                        gameBoard[6] = userTurn;
+                    }
+                    break;
+                case 7:
+                    if (gameBoard[7] == ' ') {
+                        gameBoard[7] = userTurn;
+                    }
+                    break;
+                case 8:
+                    if (gameBoard[8] == ' ') {
+                        gameBoard[8] = userTurn;
+                    }
+                    break;
+                default:
+                    System.out.println("Choose correct number");
+
+            }
         }
     }
 }
